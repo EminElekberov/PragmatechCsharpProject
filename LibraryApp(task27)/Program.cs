d21c5ad58cc9f05243b1d00fb9cf3f8294fe8bf3
EminElekberov <eminelekberov09@gmail.com>
2022-01-25 22:45:21 +0400

    update

--- LibraryApp(task27)/Program.cs
+++ LibraryApp(task27)/Program.cs
@@ -16,7 +16,7 @@ namespace LibraryApp_task27_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AdminPanel());
         }
     }
 }

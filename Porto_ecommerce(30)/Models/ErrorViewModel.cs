7fb6959d8e2e7055f4e8badb7b443448c027e253
EminElekberov <eminelekberov09@gmail.com>
2022-02-05 12:35:56 +0400

    update

---
+++ Porto_ecommerce(30)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Porto_ecommerce_30_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

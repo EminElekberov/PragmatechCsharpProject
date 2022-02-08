f9648f00ea90801d625db27c1c1ab6ac48f3a6aa
EminElekberov <eminelekberov09@gmail.com>
2022-02-08 19:36:09 +0400

    update

---
+++ ConsoleApp1/Retry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Retry
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Retry()
+        {
+
+        }
+    }
+}

5f2b18e3bc7c8dd6e5b821cf3df14911a0a00c13
EminElekberov <eminelekberov09@gmail.com>
2022-02-11 15:50:04 +0400

    update

--- DotNetTasks(game)/Program.cs
+++ DotNetTasks(game)/Program.cs
@@ -10,101 +10,79 @@ namespace DotNetTasks_game_
     {
         static void Main(string[] args)
         {
-            #region
-            //Random random = new Random();
-            //string[] words = { "man", "rat", "cow", "chicken","asdf","frfrt" };
-            //for (int i = 0; i < 2; i++)
-            //{
+            #region 1ci sual
+            //    int dogru = 0;
+            //    double Sum = 0;
+            //    double bal = 0;
+            //    int say = 0;
+            //START:
+
+            //    Random random = new Random();
+            //    string[] words = { "You can", "gain experience", "if you", "are careful", "to avoid", "empty redundancy" };
             //    int index = random.Next(words.Length);
+            //    int index2 = random.Next(words.Length);
             //    Console.WriteLine(words[index]);
+            //    Console.WriteLine(words[index2]);
+            //    List<string> newWords = new List<string>();
 
-            //}
+
+            //    for (int hak = 3; hak > 0; hak--)
+            //    {
+            //        Console.WriteLine("bir wsoz daxil edin: ");
+            //        string soz = Console.ReadLine();
+            //        say++;
+            //        if (hak != 0)
+            //        {
+            //            if (soz == words[index2].ToString())
+            //            {
+            //                if (hak == 3)
+            //                {
+            //                    bal = 6.2;
+            //                    Console.WriteLine($"tebriker ikinci sozu {say} texminde  bildiniz ve sizin baliniz {bal}");
+            //                }
+            //                else if (hak == 2)
+            //                {
+            //                    bal = 4;
+            //                    Console.WriteLine($"tebriker ikinci sozu {say} texminde bildiniz ve sizin baliniz {bal}");
+            //                }
+            //                else if (hak == 1)
+            //                {
+            //                    bal = 2;
+            //                    Console.WriteLine($"tebriker ikinci sozu {say} texminde bildiniz ve sizin baliniz {bal}");
+            //                }
+            //                dogru++;
+            //                Sum += bal;
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("dogru tapa bilmediniz");
+            //            }
+            //            newWords.Add(soz);
+
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("sizin arti qtexmin haqqiniz qalmadi");
+
+            //        }
+
+            //    }
+            //    Console.WriteLine("helede soz daxil edilsinmi? (y/n)");
+            //    if (Console.ReadKey().Key == ConsoleKey.Y)
+            //    {
+            //        goto START;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sizin statistikalariniz: ");
+            //        Console.WriteLine("soz ctluyunuz sayi: " + newWords.Count);
+            //        Console.WriteLine("dogru " + dogru);
+            //        int wrongWords=newWords.Count - dogru;
+            //        Console.WriteLine("yanlis tapilan soz cutluklerinin sayi: "+ wrongWords);
+            //        Console.WriteLine("qazanilan xal: "+Sum);
+            //    }
             #endregion
-            int dogru = 0;
-            double Sum = 0;
-            double bal = 0;
-            int say = 0;
-        START:
 
-            Random random = new Random();
-            string[] words = { "You can", "gain experience", "if you", "are careful", "to avoid", "empty redundancy" };
-            int index = random.Next(words.Length);
-            int index2 = random.Next(words.Length);
-            Console.WriteLine(words[index]);
-            Console.WriteLine(words[index2]);
-            List<string> newWords = new List<string>();
-            
-
-            for (int hak = 3; hak > 0; hak--)
-            {
-                Console.WriteLine("bir wsoz daxil edin: ");
-                string soz = Console.ReadLine();
-                say++;
-                if (hak != 0)
-                {
-                    if (soz == words[index2].ToString())
-                    {
-                        if (hak == 3)
-                        {
-                            bal = 6.2;
-                            Console.WriteLine($"tebriker ikinci sozu {say} texminde  bildiniz ve sizin baliniz {bal}");
-                        }
-                        else if (hak == 2)
-                        {
-                            bal = 4;
-                            Console.WriteLine($"tebriker ikinci sozu {say} texminde bildiniz ve sizin baliniz {bal}");
-                        }
-                        else if (hak == 1)
-                        {
-                            bal = 2;
-                            Console.WriteLine($"tebriker ikinci sozu {say} texminde bildiniz ve sizin baliniz {bal}");
-                        }
-                        dogru++;
-                        Sum += bal;
-                    }
-                    else
-                    {
-                        Console.WriteLine("dogru tapa bilmediniz");
-                    }
-                    newWords.Add(soz);
-
-                }
-                else
-                {
-                    Console.WriteLine("sizin arti qtexmin haqqiniz qalmadi");
-
-                }
-
-            }
-            Console.WriteLine("helede soz daxil edilsinmi? (y/n)");
-            if (Console.ReadKey().Key == ConsoleKey.Y)
-            {
-                goto START;
-            }
-            else
-            {
-                Console.WriteLine("sizin statistikalariniz: ");
-                Console.WriteLine("soz ctluyunuz sayi: " + newWords.Count);
-                Console.WriteLine("dogru " + dogru);
-                int wrongWords=newWords.Count - dogru;
-                Console.WriteLine("yanlis tapilan soz cutluklerinin sayi: "+ wrongWords);
-                Console.WriteLine("qazanilan xal: "+Sum);
-            }
-            
-        #region
-        //string content = "This is really fun. I am learning to code.  ";
-        //char[] list = new char[] { ' ', '.' };
-
-        //var split = content.Split(list, StringSplitOptions.RemoveEmptyEntries);
-
-
-        //Random random = new Random(split.Length);
-        //for (int i = 0; i < 2; i++)
-        //{
-        //    int index = random.Next(list.Length);
-        //    Console.WriteLine(index[i]);
-        //}
-        #endregion
 
         }
     }

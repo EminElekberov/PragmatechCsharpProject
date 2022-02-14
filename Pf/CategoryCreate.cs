40922340a3eabf32d20aef484ef00fbd7978814a
EminElekberov <eminelekberov09@gmail.com>
2022-02-14 21:53:31 +0400

    update

--- Pf/CategoryCreate.cs
+++ Pf/CategoryCreate.cs
@@ -16,22 +16,39 @@ namespace Pf
 
         private void CategoryCreate_Load(object sender, EventArgs e)
         {
-            combCategory.DropDownStyle = ComboBoxStyle.DropDownList;
-            combCategory.DataSource = parfumEntities.Categories.Select(x => new Cb_category
-            {
-                Id=x.Id,
-                Name = x.Name
-            }).ToArray();
+            CategoryChange();
         }
 
         private void CategoryChange()
         {
-           
+            combCategory.DropDownStyle = ComboBoxStyle.DropDownList;
+            combCategory.DataSource = parfumEntities.Categories.Select(x => new Cb_category
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToArray();
         }
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-           
+            string name = textNewCategrory.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("write name please");
+                return;
+            }
+            else
+            {
+                DbModel.Category category = new Category
+                {
+                    Name=name
+                };
+                parfumEntities.Categories.Add(category);
+                parfumEntities.SaveChanges();
+                CategoryChange();
+                MessageBox.Show("success");
+                textNewCategrory.Clear();
+            }
         }
 
 
@@ -39,13 +56,32 @@ namespace Pf
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-
+            string name=textNewCategrory
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-
-
+            string name = textNewCategrory.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Please write name");
+            }
+            else
+            {
+                DbModel.Category category = parfumEntities.Categories.FirstOrDefault(x => x.Name == name);
+                if (category==null)
+                {
+                    MessageBox.Show("this doesn't parfum already");
+                }
+                else
+                {
+                    parfumEntities.Categories.Remove(category);
+                    parfumEntities.SaveChanges();
+                    CategoryChange();
+                    MessageBox.Show("success deleted");
+                    textNewCategrory.Clear();
+                }
+            }
         }
     }
 }

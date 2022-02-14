fadef88ebf9531c4f77771861f75c993b28c75da
EminElekberov <eminelekberov09@gmail.com>
2022-02-14 18:55:18 +0400

    update

---
+++ Pf/CategoryCreate.cs
@@ -0,0 +1,51 @@
+﻿using Pf.DbModel;
+using Pf.Helper;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+namespace Pf
+{
+    public partial class CategoryCreate : Form
+    {
+        private ParfumEntities parfumEntities; 
+        public CategoryCreate()
+        {
+            InitializeComponent();
+            parfumEntities = new ParfumEntities();
+        }
+
+        private void CategoryCreate_Load(object sender, EventArgs e)
+        {
+            combCategory.DropDownStyle = ComboBoxStyle.DropDownList;
+            combCategory.DataSource = parfumEntities.Categories.Select(x => new Cb_category
+            {
+                Id=x.Id,
+                Name = x.Name
+            }).ToArray();
+        }
+
+        private void CategoryChange()
+        {
+           
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+     
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
---
+++ Pf/Helper/Cb_category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pf.Helper
+{
+   public class Cb_category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}

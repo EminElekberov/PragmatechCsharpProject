2cf0e38fa7a5ac13825f9947119cbcf674c3aad3
EminElekberov <eminelekberov09@gmail.com>
2022-02-17 22:15:29 +0400

    update

--- task12/Program.cs
+++ task12/Program.cs
@@ -13,6 +13,9 @@ namespace task12
             //Check ch = new Check(IsOdd);
             //ch += IsEven;
             //ch.Invoke(5);
+            
+
+
             //
             #endregion
             #region IsContainsDigit

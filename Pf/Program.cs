2f7f03ddb0bc4d4792cb51f725e28d1d803d94e2
EminElekberov <eminelekberov09@gmail.com>
2022-03-03 22:19:19 +0400

    update

--- Pf/Program.cs
+++ Pf/Program.cs
@@ -16,7 +16,7 @@ namespace Pf
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SinginParfum());
+            Application.Run(new UserActivity());
         }
     }
 }

1cdd6baf3962f4085da330057f2f0f6527097dfa
EminElekberov <eminelekberov09@gmail.com>
2022-04-09 22:16:40 +0400

    update

--- winformders15/Program.cs
+++ winformders15/Program.cs
@@ -12,6 +12,7 @@ namespace winformders15
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);

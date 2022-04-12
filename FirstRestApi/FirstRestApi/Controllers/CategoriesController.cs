872b25f4a700be1bb6876881b8e4f30f949d3170
EminElekberov <eminelekberov09@gmail.com>
2022-04-12 21:34:21 +0400

    update

---
+++ FirstRestApi/FirstRestApi/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using FirstRestApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(_context.Categories.Include(c => c.Products));
+        }
+
+
+        [HttpGet("{id}/{take}")]
+        public async Task<IActionResult> Get(int id, int take)
+        {
+            var category = await _context.Categories.Include(x => x.Products).FirstOrDefaultAsync(g => g.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category.Products.Take(take));
+        }
+    }
+}
---
+++ FirstRestApi/FirstRestApi/Model/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstRestApi.Model
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Dress"}
+                ,new Category { Id=2,Name="Shoes"}
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id=1,Name="T shirt Man",Price=5,CategoryId=1},
+                new Product { Id=2,Name="T shirt Woman",Price=15,CategoryId=1},
+                new Product { Id=3,Name="Shoes",Price=55,CategoryId=2},
+                new Product { Id=4, Name = "Classic shoes", Price = 95, CategoryId = 2 }
+                );
+        }
+    }
+}

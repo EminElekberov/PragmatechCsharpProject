310d416b526ed2072062f4eef89a2724f9d4b241
EminElekberov <eminelekberov09@gmail.com>
2022-04-27 23:06:07 +0400

    upadte

--- repeatTask/Login.cs
+++ repeatTask/Login.cs
@@ -25,7 +25,6 @@ namespace LibraryApi
         private void BtnRegister_Click(object sender, EventArgs e)
         {
         }
-
         private void BtnLogin_Click(object sender, EventArgs e)
         {
 
